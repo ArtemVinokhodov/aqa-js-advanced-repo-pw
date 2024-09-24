85b46d6b41e13821d43ed7981e36435ccbafcde2
Artem_Vinokhodov <a.vinohod@gmail.com>
2024-09-24 18:05:17 +0200

    rename folder

--- tests/example.test.js
+++ tests/example.test.js
@@ -4,4 +4,5 @@ const { test, expect } = require('@playwright/test');
 test('basic test', async ({ page }) => {
     await page.goto('https://example.com');
     await expect(page).toHaveTitle('Example Domain');
+    await expect(page).toHaveTitle('Example Domain');
 });

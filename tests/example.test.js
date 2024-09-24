987654a5558f523c23c61b993ef46bfcdf9c5d8e
Artem_Vinokhodov <a.vinohod@gmail.com>
2024-09-24 15:33:52 +0200

    lockal set pw

--- tests/example.test.js
+++ tests/example.test.js
@@ -3,5 +3,5 @@ const { test, expect } = require('@playwright/test');
 
 test('basic test', async ({ page }) => {
     await page.goto('https://example.com');
-    await expect(page).toHaveTitle(/Example Domain/);
+    await expect(page).toHaveTitle('Example Domain');
 });

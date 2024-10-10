aaa72185fc22f525e73559a16cdd2a1a08d1292e
Artem_Vinokhodov <a.vinohod@gmail.com>
2024-10-10 10:48:27 +0200

    path not found

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from '@playwright/test';
 import * as dotenv from 'dotenv';
-dotenv.config({ path: './path/to/your/.env' });
+dotenv.config({ path: './.env' });
 
 export default defineConfig({
   testDir: './tests',
